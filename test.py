dfd953eab2e7acaf712149dca1853e4071aba000
a-hopps <alicia.hopper29@gmail.com>
2024-08-14 11:24:11 -0700

    Chnages to Dataset - new files and converter program

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ model = YOLO("yolov8n.yaml")
 # Load a pretrained YOLO model (recommended for training)
 model = YOLO("yolov8n.pt")
 
-# Train the model using the 'coco8.yaml' dataset for 100 epochs
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+# Train the model using the 'coco.yaml' dataset for 100 epochs
+results = model.train(data="coco.yaml", epochs=100, imgsz=640)
 
 # Evaluate the model's performance on the validation set
 results = model.val()
