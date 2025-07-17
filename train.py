fb57d778be17d9a7fd91a1681115a2c020106d4f
River Johnson <river32_guest@rita.cecs.pdx.edu>
2025-07-17 06:01:06 -0700

    100 -> 600 epochs

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 """
-This program is used to train a YOLO model for 100 epochs on ECP data
+This program is used to train a YOLO model for 600 epochs on ECP data
 
 Written by Allie Hopper, 2024
 Edited by River Johnson, 2025
@@ -24,23 +24,25 @@ print(f"Using {device} device")
 print(torch.__version__)
 # --------------------------------- #
 
+# I never want to accidentally run this using the CPU. Disable this check to run on a cpu
+if device == "cpu":
+    exit()
 
-# Load a pretrained YOLO model from the last training batch (if first training batch, use "yolov8s.pt") 
+# Load a pretrained YOLO model. For initial training, use "yolov8s.pt"
 # model = YOLO("runs/detect/train6/weights/best.pt")
 model = YOLO("yolov8s.pt")
 
-# Train the model using the 'ECP.yaml' dataset for 100 epochs - set resume=True to resume interrupted training
+# Train the model using the 'ECP.yaml' dataset for 600 epochs - set resume=True to resume interrupted training
 # Original: results = model.train(data="2024-PSU-REU/ECP.yaml", epochs=100, imgsz=640, workers=16, resume=True)
 # Small test: results = model.train(data="ECP.yaml", epochs=20, imgsz=640, workers=16, resume=False, device=0, cache=False)
 # Current version:
 results = model.train(data="ECP.yaml", epochs=600, imgsz=640, workers=16, resume=False, device=0, cache=False)
 
 # Evaluate the model's performance on the validation set
-results = model.val()
+metrics = model.val()
 
 # Perform object detection on an image using the model
-# results = model("image.jpg")
+results = model.predict("image.jpg")
 
 # Export the model to ONNX format
 success = model.export(format="onnx")
-
