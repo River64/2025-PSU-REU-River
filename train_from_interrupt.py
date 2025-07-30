2b7f2f5340ca5d247b67255aa45199b9b577e4bc
River Johnson <river32_guest@rita.cecs.pdx.edu>
2025-07-30 10:36:24 -0700

    Now uses command-line arguments

--- train_from_interrupt.py
+++ train_from_interrupt.py
@@ -9,9 +9,17 @@ Edited by River Johnson, 2025
 
 from ultralytics import YOLO
 import torch
+import sys
 
+if (len(sys.argv) < 2):
+    print("Usage: python3 train_from_interrupt.py <folder to resume from>\n
+          Example: python3 train_from_interrupt.py train19\n
+          These folders are in runs/detect, made when any training run is started")
+    exit()
+else:
+    resume_folder = sys.argv[1]
 
-# ----------- DEBUGGING ----------- #
+# ----------- CPU CHECK ----------- #
 
 # Get cpu, gpu or mps device for training.
 device = (
@@ -21,20 +29,21 @@ device = (
     if torch.backends.mps.is_available()
     else "cpu"
 )
+
 print(f"Using {device} device")
-
 print(torch.__version__)
-# --------------------------------- #
 
-# I never want to accidentally run this using the CPU. Disable this check to run on a cpu
+# I never want to accidentally run this using the CPU
+# Disable this check to run on a CPU
 if device == "cpu":
     exit()
+# --------------------------------- #
 
 # Load a pretrained YOLO model. This is the interrupted training run to resume from
-# Depending on what run got interrupted, the folder name will be different.
+# Depending on what run got interrupted, the folder name will be different, so that's a command line argument.
 # By default YOLO names the training run folders train#, but there are ways to put in custom names.
 # Also, YOLO makes "runs" and its subdirectories in whatever folder you run this script by default.
-model = YOLO("runs/detect/train27/weights/last.pt")
+model = YOLO("runs/detect/" + resume_folder + "/weights/last.pt")
 
 # Train the model using the 'ECP.yaml' dataset for 200 epochs
 results = model.train(data="ECP.yaml", epochs=200, imgsz=640, workers=16, device=0, cache=True, resume=True, patience=50)
