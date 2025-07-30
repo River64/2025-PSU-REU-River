a3ac8dd74eb6ae088e9a5b8c39cd71c5d09fa489
River Johnson <river32_guest@rita.cecs.pdx.edu>
2025-07-30 10:55:31 -0700

    add start-of-file comments

--- test_cuda_available.py
+++ test_cuda_available.py
@@ -1,5 +1,11 @@
 import torch
 
+"
+A small script to test if CUDA is available for using the GPU.
+This check is run in train.py itself, but having it on its own
+is also useful.
+"
+
 # Get cpu, gpu or mps device for training.
 device = (
     "cuda"
